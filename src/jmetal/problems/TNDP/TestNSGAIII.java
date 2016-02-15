c7c0295c22a00d7acfc6151658d944340b797e24
Ali Nayeem <nayeem007@gmail.com>
2016-02-15 08:58:38 +0600

    Added RouteSetTEOMutation: Triangular Inequality based on Edge overlapping, tried from any end at random

--- src/jmetal/problems/TNDP/TestNSGAIII.java
+++ src/jmetal/problems/TNDP/TestNSGAIII.java
@@ -14,6 +14,7 @@ import jmetal.operators.crossover.RouteSetCrossover;
 import jmetal.operators.mutation.RouteSetAddDelMutation;
 import jmetal.operators.mutation.RouteSetDelMutation;
 import jmetal.operators.mutation.RouteSetTELMutation;
+import jmetal.operators.mutation.RouteSetTEOMutation;
 //import jmetal.operators.selection.SelectionFactory;
 
 /**
@@ -45,7 +46,7 @@ public class TestNSGAIII
         algorithm.setInputParameter("maxGenerations", 700);
 
         crossover = new RouteSetCrossover(null);
-        mutation = new RouteSetTELMutation(null);
+        mutation = new RouteSetTEOMutation(null);
         selection = new RetativeTournamentSelection(null);
         // Add the operators to the algorithm
         algorithm.addOperator("crossover", crossover);
